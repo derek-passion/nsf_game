874f6d77fc2dd8c311f3369354ec73ea31b7efc2
Jiangtao Hu <boris.hu@gmail.com>
2024-11-17 09:35:48 -0800

    Add upper case and number support

--- client/scenes/Welcome.js
+++ client/scenes/Welcome.js
@@ -6,14 +6,21 @@ export default class Welcome extends Phaser.Scene {
     Register allowed keys
     */
   init() {
-    var alpha = "abcdefghijklmnopqrstuvwxyz".split("").join(",");
-    this.keys = this.input.keyboard.addKeys(alpha);
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const characters = letters + letters.toLowerCase() + numbers;
+
+    const keys = characters.split("").join(",");
+    console.log(keys);
+    this.keys = this.input.keyboard.addKeys(keys);
+
     this.backspace = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.BACKSPACE
     );
     this.enter = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.ENTER
     );
+
   }
 
 
@@ -29,25 +36,31 @@ export default class Welcome extends Phaser.Scene {
     });
     this.text.setOrigin(0.5);
     this.name = "";
+
+    // Create a keyboard input
+    this.input.keyboard.on('keydown', (event) => {
+        // Check if the key is a letter or backspace
+        if (event.key.length === 1 || event.key === 'Backspace') {
+            if (event.key === 'Backspace') {
+                this.name = this.name.slice(0, -1);
+            } else {
+                this.name += event.key;
+            }
+
+            // Update the display text
+            this.text.setText(this.name);
+        }
+    });
   }
 
     /*
     Poll for keyboard keys to display name, and for enter to go to game scene.
     */
   update() {
-    for (const key of Object.keys(this.keys)) {
-      if (Phaser.Input.Keyboard.JustDown(this.keys[key])) {
-        if (this.name.length < 15) {
-          this.name += key;
-        }
-      }
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.backspace)) {
-      this.name = this.name.substring(0, this.name.length - 1);
-    }
     if (Phaser.Input.Keyboard.JustDown(this.enter)) {
       this.scene.start("playgame", this.name);
     }
     this.text.setText(this.welcome_text + this.name);
+    console.log("v6");
   }
 }
